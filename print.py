f2e7da87c38ec16b45e51cba1e2fc426c6a13332
FELIPE DABES GUIMARAES <felipedabes@gmail.com>
2022-11-25 19:48:56 -0300

    ajuste_pasta

---
+++ print.py
@@ -0,0 +1,5 @@
+# \r\n -> CRLF
+print(12, 34, sep='&&', end='\r\n')
+print(56, 34, end='\n\n\n\n')
+print(56, 34, end='\n')
+
---
+++ string.py
@@ -0,0 +1,10 @@
+print(1234)
+print('Luiz Otávio')
+print("Luiz Otávio")
+
+#CARACTER DE ESCAPE \
+print("Luiz \"Otávio")
+
+# r -> para expressões regulares
+print(r"Luiz \"Otávio\"")
+print('Explícito', 'é', 'melhor " do que implícito')
