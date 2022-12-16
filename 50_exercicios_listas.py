27a1ece74741cd3de998b333bfeb38ae70b78c12
FELIPE DABES GUIMARAES <felipedabes@gmail.com>
2022-12-16 11:51:49 -0300

    Exercicio de lista de compras finalizado

--- 50_exercicios_listas.py
+++ 50_exercicios_listas.py
@@ -1,40 +1,42 @@
-# inserir apagar listar
+import os
 resposta = 'Inicio'
-lista_de_compras = list([])
+lista_de_compras = []
 
-
+print('Seja bem vindo(a) ao aplicativo LISTA DE COMPRAS!!!\n')
 while resposta != 's':
     resposta = input('Selecione uma opção: \n [i]nserir  [a]pagar  [l]istar [s]air: ').lower().strip()
     if resposta == 's':
-        print('Saindo da lista de compras. Obrigado por utilizar nosso aplicativo.')
+        os.system('clear')
+        print('\n Saindo da lista de compras. Obrigado por utilizar nosso aplicativo.')
         continue
     elif resposta == 'i':
-        produto = input('Qual o produto você deseja inserir na sua lista de compras?').capitalize().strip()
+        produto = input('Qual o produto você deseja inserir na sua lista de compras?\n').capitalize().strip()
         if produto in lista_de_compras:
-            print('Esse produto já consta na lista de compras')
+            os.system('clear')
+            print('Esse produto já consta na lista de compras.\n')
         else:
             lista_de_compras.append(produto)
         continue
     elif resposta == 'l':
+        os.system('clear')
         for indice, nome_produto in enumerate(lista_de_compras):
             print(indice , '-' , nome_produto)
+        if lista_de_compras == []:
+            print('Não há nenhum item na lista de compras!\n')
 
-#    -----------------AJUSTAR AQUI -------------------
-    # elif resposta == 'a':
-    #     item_apagar = input('Digite o código ou o nome do item que deseja apagar: ').capitalize().strip()
-    #     try:
-    #         item_apagar_int = int(item_apagar)
-    #         for indice, nome_produto in enumerate(lista_de_compras):
-    #             if indice == item_apagar_int:
-    #                 del nome_produto[indice], indice
-    #             print(indice, '-', nome_produto)
-    #     except:
-    #         for indice, nome_produto in enumerate(lista_de_compras):
-    #             if nome_produto == item_apagar:
-    #                 del nome_produto[indice], indice
-    #             print(indice, '-' , nome_produto)
-#    -----------------AJUSTAR AQUI -------------------
-
+    elif resposta == 'a':
+        item_apagar = input('Digite o código ou o nome do item que deseja apagar: ').capitalize().strip()
+        try:
+            item_apagar_int = int(item_apagar)
+            for indice, nome_produto in enumerate(lista_de_compras):
+                if indice == item_apagar_int:
+                    del lista_de_compras[indice]                 
+        except :
+            for indice, nome_produto in enumerate(lista_de_compras):
+                if nome_produto == item_apagar:
+                    del lista_de_compras[indice]
+                  
     else:
+        os.system('clear')
         print('Resposta inválida, tente novamente!')
         continue
