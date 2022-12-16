0846419716162c4ca31b2eddce7df786427422f9
FELIPE DABES GUIMARAES <felipedabes@gmail.com>
2022-12-16 17:55:13 -0300

    operacao ternaria

--- 55_operacao_ternaria.py
+++ 55_operacao_ternaria.py
@@ -1,2 +1,11 @@
 varial = 'Valor' if True else 'Outro valor'
 print('Valor' if True else 'Outro valor')
+
+digito = 1
+novo_digito = 0 if digito > 9 else digito
+print(novo_digito)
+
+novo_digito = digito if digito <= 9 else 0
+print(novo_digito)
+
+print('Valor' if False else 'Outro valor' if False else 'Fim')
---
+++ 56_exercicio_gerar_digito_CPF.py
@@ -0,0 +1,6 @@
+# CPF TESTE: 746.824.890-70
+
+cpf = '746.824.890-70'
+
+separador_numero_cpf = cpf.split()
+print(separador_numero_cpf)
